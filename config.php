2fc528d4acdc997cb9d07ed573d4660fb2274a28
Research <rangamanisha11@gmail.com>
2023-01-16 11:55:05 -0500

    Styles added to register and login, pending mysql and crud

---
+++ config.php
@@ -0,0 +1,28 @@
+  <?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+        $conn = mysqli_connect('localhost', 'root', 'root', 'test1') or die("Connection Failed:" .mysqli_connect_error());
+        
+
+        if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['gender'])) {
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $gender = $_POST['gender'];
+
+            $sql = "INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `gender`) VALUES ('$firstname', '$lastname', '$email', '$password', '$gender')";
+            $query = mysqli_query($conn,$sql);
+            if($query){
+                echo 'Entry Successful';
+
+            }
+            else {
+                echo 'Error Occurred';
+            }
+
+
+        }
+    }
+
+?> 
